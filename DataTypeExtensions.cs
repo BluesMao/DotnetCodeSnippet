08634a9bf070c8eac65be2d36ee246b9ee1378a9
毛远宙 <516870524@qq.com>
2018-10-23 10:02:06 +0800

    Initial commit

---
+++ DataTypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sov.IronPay.Core
+{
+    public static class DataTypeExtensions
+    {
+        public static R ChangeType<T, R>(this T Object, R DefaultValue = default(R))
+        {
+            if (Object == null || Convert.IsDBNull(Object))
+                return DefaultValue;
+            if ((Object as string) != null)
+            {
+                string ObjectValue = Object as string;
+                if (typeof(R).IsEnum)
+                    return (R)System.Enum.Parse(typeof(R), ObjectValue, true);
+                if (string.IsNullOrEmpty(ObjectValue))
+                    return DefaultValue;
+            }
+            if ((Object as IConvertible) != null)
+            {
+                Type destination =
+                    typeof(R).IsGenericType && typeof(R).GetGenericTypeDefinition() == typeof(Nullable<>) ?
+                        Nullable.GetUnderlyingType(typeof(R)) : typeof(R);
+                return (R)Convert.ChangeType(Object, destination);
+            }
+            if (typeof(R).IsAssignableFrom(Object.GetType()))
+                return (R)(object)Object;
+            return DefaultValue;
+        }
+    }
+}
---
+++ GenericObjectExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sov.IronPay.Core
+{
+    public static class GenericObjectExtensions
+    {
+        public static T Execute<T>(this Func<T> Function, int Attempts = 3, int RetryDelay = 0, int TimeOut = int.MaxValue)
+        {
+            if (Function == null)
+                throw new ArgumentNullException("Function");
+            Exception Holder = null;
+            long Start = System.Environment.TickCount;
+            while (Attempts > 0)
+            {
+                try
+                {
+                    return Function();
+                }
+                catch (Exception e) { Holder = e; }
+                if (System.Environment.TickCount - Start > TimeOut)
+                    break;
+                Thread.Sleep(RetryDelay);
+                --Attempts;
+            }
+            throw Holder;
+        }
+
+        public static void Execute(this Action Action, int Attempts = 3, int RetryDelay = 0, int TimeOut = int.MaxValue)
+        {
+            if (Action == null)
+                throw new ArgumentNullException("Function");
+            Exception Holder = null;
+            long Start = System.Environment.TickCount;
+            while (Attempts > 0)
+            {
+                try
+                {
+                    Action();
+                }
+                catch (Exception e) { Holder = e; }
+                if (System.Environment.TickCount - Start > TimeOut)
+                    break;
+                Thread.Sleep(RetryDelay);
+                --Attempts;
+            }
+            if (Holder != null)
+                throw Holder;
+        }
+    }
+}
